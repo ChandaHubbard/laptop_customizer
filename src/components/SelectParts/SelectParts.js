5faf010a4a8869f79bb67ea1287669ffd02f1a51
Chanda Hubbard <56906759+ChandaHubbard@users.noreply.github.com>
2020-03-06 00:04:51 -0800

    Add files via upload

--- src/components/SelectParts/SelectParts.js
+++ src/components/SelectParts/SelectParts.js
@@ -12,10 +12,7 @@ function SelectParts(props) {
     {props.features}
     <div className="summary__option" key={props.featureHash}>
         <div className="summary__option__label">{props.feature} </div>
-        {/* <div className="summary__option__value">{props.selectedOption.name}</div> */}
-        {/* <div className="summary__option__cost">
-          {props.USCurrencyFormat.format(props.selectedOption.cost)}
-        </div> */}
+        
       </div>
     <ListParts
     features={props.features}
